8ec2cd02552e28536caccbb0d3c6f648c517e623
Joshua Hamilton <jouhou@posteo.de>
2020-12-22 15:05:51 +0100

    Kleinigkeiten und bissl UI

--- src/main/java/de/othr/sw/hamilton/repository/UserRepository.java
+++ src/main/java/de/othr/sw/hamilton/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByEmail(String email);
+    List<User> findByUsername(String email);
 }
--- src/main/java/de/othr/sw/hamilton/service/UserService.java
+++ src/main/java/de/othr/sw/hamilton/service/UserService.java
@@ -5,6 +5,7 @@ import de.othr.sw.hamilton.entity.Customer;
 import de.othr.sw.hamilton.entity.User;
 import de.othr.sw.hamilton.repository.BankAccountRepository;
 import de.othr.sw.hamilton.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -45,7 +46,13 @@ public class UserService implements Serializable, UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(s).stream().findAny().orElseThrow(() -> new UsernameNotFoundException("User mit Email nicht gefunden: " + s));
+        User user = userRepository.findByUsername(s).stream().findAny().orElseThrow(() -> new UsernameNotFoundException("User mit Email nicht gefunden: " + s));
         return user;
+    }
+
+    public UserDetails getCurrentUser() {
+        //TODO sicherstellen dass jemand eingeloggt ist?
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return loadUserByUsername(username);
     }
 }
